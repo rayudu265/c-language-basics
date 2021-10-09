11b4698b59ab896852234bcbd18be922aeaf7126
rayudu265 <92150083+rayudu265@users.noreply.github.com>
2021-10-09 15:04:56 +0530

    Add files via upload

---
+++ area of rec.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int length,breadth,area;
+	printf("\nenter the length");
+	scanf("%d",&length);
+	printf("\nenter the breadth");
+	scanf("%d",&breadth);
+	area=length*breadth;
+	printf("%d",area);
+}
