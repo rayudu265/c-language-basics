46a5b0021a008e3160ea4d3301878e26cb4c530c
rayudu265 <92150083+rayudu265@users.noreply.github.com>
2021-10-09 15:00:28 +0530

    Add files via upload

---
+++ realtional operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=6,b=8;
+	printf("%d\n",a<b);
+	printf("%d\n",a<=b);
+	printf("%d\n",a>b);
+	printf("%d\n",a>=b);
+	printf("%d\n",a==b);
+	printf("%d\n",a=!b);
+}
