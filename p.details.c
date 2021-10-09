bd1d5b9eb9f13336857df3835de9fe62ad42671f
rayudu265 <92150083+rayudu265@users.noreply.github.com>
2021-10-09 14:59:59 +0530

    Add files via upload

---
+++ p.details.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+printf("name:M.Thirupathiraidu\n");
+printf("father name:m.chittibabu\n");
+printf("mother name:m.venkatalaxmi\n");
+printf("sister name:m.uma maheshwari\n");
+
+}
