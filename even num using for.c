5f11164b0780c9720ce025afce6e131d6c6d70ce
rayudu265 <92150083+rayudu265@users.noreply.github.com>
2021-10-18 14:44:21 +0530

    Add files via upload

---
+++ even num using for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int s;
+  for(s=1;s<=10;s++)
+  {
+  	if(s%2==0)
+  	{
+  	 	printf("%d",s);	
+	  }
+  
+  }
+}
---
+++ for loop 2nd variatiion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	for(;a<=50;)
+	{ printf("aditya\n");
+	 a++;
+	
+	}
+}
