17cafd2f4e2619c83936b80f8249c4f32a95089d
rayudu265 <92150083+rayudu265@users.noreply.github.com>
2021-11-15 13:25:59 +0530

    Add files via upload

---
+++ rev of array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,arr[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=n-1;i>=0;i--)
+	{
+		printf("%d",arr[i]);
+	}
+	
+}
